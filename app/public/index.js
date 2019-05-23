89ea0bfbdb94ff1199f45d84d8d14b029a74f818
Kristen De La Rosa <delarosa.kristen@gmail.com>
2019-05-22 20:45:36 -0400

    jquery on click added for submit aand post for loop fixed

--- app/public/index.js
+++ app/public/index.js
@@ -1,1 +1,13 @@
 // create on click for submit button that does ajax to the server
+$("#submit").on("click", function(){
+  console.log("Hello!!!");
+  var userData = {
+    name: $("#name").val(),
+    photo: $("#photo").val(),
+    score: [$("#q1").val(),$("#q2").val(),$("#q3").val(),$("#q4").val(),$("#q5").val(),$("#q6").val(),$("#q7").val(),$("#q8").val(),$("#q9").val(),$("#q10").val()]
+  };
+
+  $.post("/api/friends", userData, function(data){
+    console.log("Data: "+ JSON.stringify(data));
+  })
+});
